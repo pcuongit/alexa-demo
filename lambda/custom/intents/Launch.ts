e62e0126093c3641a8f995e989c49a925b3e2f31
pcuong1 <pcuong1@gmail.com>
2020-10-05 23:49:18 +0700

    push 2

--- lambda/custom/intents/Launch.ts
+++ lambda/custom/intents/Launch.ts
@@ -2,13 +2,15 @@ import { RequestHandler, HandlerInput } from 'ask-sdk-core';
 import { RequestTypes, Strings } from '../utilities/constants';
 import { IsType } from '../utilities/helpers';
 import i18n from 'i18next';
-
+import { getApiInfo } from '../api/ApiHandler';
 export const launch: RequestHandler = {
   canHandle(handlerInput: HandlerInput) {
     return IsType(handlerInput, RequestTypes.Launch);
   },
   handle(handlerInput: HandlerInput) {
     // const speechText = i18n.t(Strings.WELCOME_MSG);
+    const apiResponse = getApiInfo('ZTM1ODg0YmY5NDNmNjY0MmU4YTJlMjli');
+    console.log('handle -> apiResponse', apiResponse);
     const speechText = 'Im opening';
     console.log('handle -> speechText', speechText);
 
